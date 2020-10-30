4b82e6dd0797de8249b6432758a65749fee5a054
laurentvac <laurentvac80@gmail.com>
2020-10-30 16:33:55 +0100

    Part1

---
+++ assets/js/style.js
@@ -0,0 +1,34 @@
+//    exo 1-->
+ let message = "ceci est mon message";
+ alert(message);
+
+ // exo 2-->
+
+
+
+ let firstPart = "Ceci est le début de mon message";
+ let lastPart = "Ceci est la fin de mon message";
+ alert(firstPart + " " + lastPart);
+ // exo 3-->
+
+        //  let name = "Jean";
+        //  let Age = 30;
+        //  let city = "Amiens";
+        //  //    let phrase = " bonjour, je m'appelle " + name + ", j'ai " + Age + " ans et j'habite " + city;
+        //  alert("bonjour, je m'appelle" + name + ", j'ai " + Age + " ans et j'habite " + city);
+         //-->
+ //exo 4 prompt https://waytolearnx.com/2019/06/boites-de-dialogue-alert-confirm-et-prompt-javascript.html-->
+ let name = prompt("validez votre nom", "laurent") ;
+
+ let age = prompt("age", 122 );
+ let city = prompt("ville", "Amiens");
+ alert("je m'appelle " + name + " j'ai " + age + " ans " + " j'habite à " + city);
+
+
+
+//exo 5 -->
+var x = prompt("donne un nombre");
+var y = prompt("donne un second numeros",);
+   var result = Number(x) + Number(y);
+   console.log(x + " + " + y + " = " + result);
+
